773cef255d012a50c8dd7d6ee409ea081667b5e0
Sean Pollock <seanpollock3344@gmail.com>
2016-11-22 12:08:36 -0600

    scaffolding and initializing main html

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ var build_dir = path.resolve(__dirname, 'app/build')
 var app_dir = path.resolve(__dirname, 'app/ui')
 
 var config = {
-  entry: app_dir + '/index.js',
+  entry: app_dir + '/main.js',
   output: {
     path: build_dir,
     filename: 'bundle.js'
