552f890f8976458a60dab8c840ea3f3147c2f788
Sean Pollock <seanpollock3344@gmail.com>
2016-11-22 14:37:56 -0600

    App is main react component

--- app/ui/main.js
+++ app/ui/main.js
@@ -1,1 +1,14 @@
-console.log('hello world')
+import React from 'react'
+import {render} from 'react-dom'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        TX Exchange
+      </div>
+    )
+  }
+}
+
+render(<App />, document.getElementById('app'))
