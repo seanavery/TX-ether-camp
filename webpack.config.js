1432db4e6d7b0ecb4389cffca40917937090f1b2
Sean Pollock <seanpollock3344@gmail.com>
2016-11-22 12:03:04 -0600

    config object for webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+
+var webpack = require('webpack')
+var path = require('path')
+
+var build_dir = path.resolve(__dirname, 'app/build')
+var app_dir = path.resolve(__dirname, 'app/ui')
+
+var config = {
+  entry: app_dir + '/index.js',
+  output: {
+    path: build_dir,
+    filename: 'bundle.js'
+  },
+  module : {
+  loaders : [
+    {
+      test : /\.js?/,
+      include : app_dir,
+      loader : 'babel',
+      query: {
+        presets: ['es2015', 'react']
+      }
+    }
+  ]
+  }
+}
+
+module.exports = config
