c005ff3b1772c1d0d3a73c0f14572b016902f463
Sean Pollock <seanpollock3344@gmail.com>
2016-11-22 15:17:21 -0600

    rendere AskLedger in AskBox

--- app/ui/components/AskBox.js
+++ app/ui/components/AskBox.js
@@ -2,12 +2,14 @@ import React from 'react'
 import {render} from 'react-dom'
 
 import AskForm from './ask_box/AskForm'
+import AskLedger from './ask_box/AskLedger'
 
 class AskBox extends React.Component {
   render() {
     return (
       <div>
         <AskForm />
+        <AskLedger />
       </div>
     )
   }
---
+++ app/ui/components/ask_box/AskLedger.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {render} from 'react-dom'
+
+class AskLedger extends React.Component {
+  render() {
+    return (
+      <div>
+        AskLedger here
+      </div>
+    )
+  }
+}
+
+module.exports = AskLedger
