fe9c43233088d48815b6cd3f83c5cc7b5840c6ee
Sean Pollock <seanpollock3344@gmail.com>
2016-11-23 17:15:02 -0600

    form setup for asks

--- app/build/bundle.js
+++ app/build/bundle.js
@@ -41845,7 +41845,7 @@
 	
 	var _reactDom = __webpack_require__(/*! react-dom */ 32);
 	
-	var _AskForm = __webpack_require__(/*! ./ask_box/AskForm */ 434);
+	var _AskForm = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./ask_box/AskForm\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
 	
 	var _AskForm2 = _interopRequireDefault(_AskForm);
 	
@@ -41888,65 +41888,7 @@
 	module.exports = AskBox;
 
 /***/ },
-/* 434 */
-/*!**********************************************!*\
-  !*** ./app/ui/components/ask_box/AskForm.js ***!
-  \**********************************************/
-/***/ function(module, exports, __webpack_require__) {
-
-	'use strict';
-	
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-	
-	var _react = __webpack_require__(/*! react */ 1);
-	
-	var _react2 = _interopRequireDefault(_react);
-	
-	var _reactDom = __webpack_require__(/*! react-dom */ 32);
-	
-	var _reactBootstrap = __webpack_require__(/*! react-bootstrap */ 178);
-	
-	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-	
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-	
-	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-	
-	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-	
-	var AskForm = function (_React$Component) {
-	  _inherits(AskForm, _React$Component);
-	
-	  function AskForm(props) {
-	    _classCallCheck(this, AskForm);
-	
-	    var _this = _possibleConstructorReturn(this, (AskForm.__proto__ || Object.getPrototypeOf(AskForm)).call(this, props));
-	
-	    _this.state = {
-	      price: [],
-	      amount: []
-	    };
-	
-	    _this.handleSubmit = _this.handleSubmit.bind(_this);
-	    return _this;
-	  }
-	
-	  _createClass(AskForm, [{
-	    key: 'handleSubmit',
-	    value: function handleSubmit() {}
-	  }, {
-	    key: 'render',
-	    value: function render() {
-	      return _react2.default.createElement('form', { onSubmit: this.handleSubmit });
-	    }
-	  }]);
-	
-	  return AskForm;
-	}(_react2.default.Component);
-	
-	module.exports = AskForm;
-
-/***/ },
+/* 434 */,
 /* 435 */
 /*!************************************************!*\
   !*** ./app/ui/components/ask_box/AskLedger.js ***!
--- app/ui/components/ask_box/AskForm.js
+++ app/ui/components/ask_box/AskForm.js
@@ -5,12 +5,18 @@ import {FormGroup, ControlLabel, FormControl, Button} from 'react-bootstrap'
 class AskForm extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {
-      price: [],
-      amount: []
-    }
 
+    this.handlePriceChange = this.handlePriceChange.bind(this)
+    this.handleAmountChange = this.handleAmountChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handlePriceChange() {
+    console.log(e.target.value)
+    
+  }
+}
+  handleAmountChange() {
+
   }
 
   handleSubmit() {
@@ -20,6 +26,17 @@ class AskForm extends React.Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit} >
+        <FormGroup>
+          <ControlLabel>Price:</ControlLabel>
+          <FormControl onChange={this.handlePriceChange} />
+        </FormGroup>
+        <FormGroup>
+          <ControlLabel>Price:</ControlLabel>
+          <FormControl onChange={this.handleAmountChange} />
+        </FormGroup>
+        <Button type='submit'>
+          SUBMIT ASK
+        </Button>
       </form>
     )
   }
