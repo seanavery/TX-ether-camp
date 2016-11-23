a2862c0ca70690a3b5f8859f6bf94adfd5f6f9dc
Sean Pollock <seanpollock3344@gmail.com>
2016-11-22 19:25:46 -0600

    porting over AskEscrow

---
+++ dapp/contracts/BidEscrowTX.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.0;
+
+contract BidEscrowTX {
+    struct Bid {
+        uint price;
+        uint amount;
+    }
+
+    Bid public bid;
+    address public exchange_address;
+
+    function BidEscrowTX(uint _price, uint _amount, address _exchange) {
+        bid.price = _price;
+        bid.amount = _amount;
+        exchange_address = _exchange;
+    }
+
+    modifier sufficientFunds() {
+        if(msg.value < bid.price * bid.amount) throw;
+        _;
+    }
+
+    function submitToExchange() returns (bool) {
+        ExchangeTX e = ExchangeTX(exchange_address);
+        e.submitBid(bid.price, bid.amount);
+        return true;
+    }
+}
