be5b18f195d8113031610d0d2627d8c6c713e7de
Sean Pollock <seanpollock3344@gmail.com>
2016-11-23 16:06:17 -0600

    adding in update for immutable functionality

--- app/ui/components/BidBox.js
+++ app/ui/components/BidBox.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import {render} from 'react-dom'
+import update from 'react-addons-update'
 
 import BidForm from './bid_box/BidForm'
 import BidLedger from './bid_box/BidLedger'
 
 class BidBox extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      price : [],
+      amount : []
+    }
+    this.submitBid = this.submitBid.bind(this)
+  }
+
+  submitBid(price, amount) {
+
+    this.setState{
+
+    }
+  }
+
   render() {
     return (
       <div>
-        <BidForm />
+        <BidForm submitBid={this.submitBid}/>
         <BidLedger />
       </div>
     )
